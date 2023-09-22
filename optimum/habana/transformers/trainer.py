d51a873cbad89ea9d82eaca96390d0b718570784
Liu, Mingzhi <mingzhi.liu@intel.com>
2023-09-22 06:22:34 +0000

    add record_shapes arg for profiling

--- optimum/habana/transformers/trainer.py
+++ optimum/habana/transformers/trainer.py
@@ -772,8 +772,9 @@ class GaudiTrainer(Trainer):
             self.log_evaluate_save_time = 0
         else:
             self.log_evaluate_save_time = None
+            # Disable record_shapes to reduce memory usage in profiling.
+            hb_profiler = HabanaProfile(warmup=self.args.profiling_warmup_steps, active=self.args.profiling_steps, record_shapes=self.args.profiling_record_shapes)
 
-        hb_profiler = HabanaProfile(warmup=self.args.profiling_warmup_steps, active=self.args.profiling_steps)
         hb_profiler.start()
 
         total_batched_samples = 0
